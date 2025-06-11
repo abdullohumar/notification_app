419131780417f1c03f7c874bfba350f783a3b340
Abdulloh Umar <iniautok@gmail.com>
2025-06-11 08:58:12 +0700

    pending notification state

--- lib/providers/local_notification_provider.dart
+++ lib/providers/local_notification_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:notification_app/services/local_notification_service.dart';
 
 class LocalNotificationProvider extends ChangeNotifier {
@@ -9,6 +10,8 @@ class LocalNotificationProvider extends ChangeNotifier {
   int _notificationId = 0;
   bool? _permission = false;
   bool? get permission => _permission;
+
+  List<PendingNotificationRequest> pendingNotificationRequests = [];
 
   Future<void> requestPermissions() async {
     _permission = await flutterNotificationService.requestPermissions();
@@ -28,5 +31,21 @@ class LocalNotificationProvider extends ChangeNotifier {
   void showBigPictureNotification() {
     _notificationId += 1;
     flutterNotificationService.showBigPictureNotification(id: _notificationId, title: "New big picture notification", body: "This big picture notification with id $_notificationId", payload: "This is a payload from big picture notification with id $_notificationId");
+  }
+
+  void scheduleDailyTenAMNotification() {
+    _notificationId += 1;
+    flutterNotificationService.scheduleDailyTenAMNotification(
+      id: _notificationId
+    );
+  }
+  Future<void> checkPendingNotificationRequests(BuildContext context) async {
+    pendingNotificationRequests = await flutterNotificationService.pendingNotificationRequest();
+    notifyListeners();
+  }
+
+  Future<void> cancelNotification(int id) async {
+    await flutterNotificationService.cancelNotification(id);
+    notifyListeners();
   }
 }
